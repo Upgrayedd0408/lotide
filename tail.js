42141e1eb4f0a04f92b32d84a61591db4e0b6bd6
Upgrayedd0408 <michaellowry89@gmail.com>
2023-11-18 07:31:19 +0000

    added tests

--- tail.js
+++ tail.js
@@ -10,8 +10,17 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-const words = tail(["Buddy", "Maverick", "Myles"]);
+let fam = ["Buddy", "Maverick", "Myles"];
+tail(fam);
+assertEqual(fam.length, 3);
+
+let words = tail(["Buddy", "Maverick", "Myles"]);
 assertEqual(words.length, 2);
 assertEqual(words[0], "Maverick");
 assertEqual(words[1], "Myles");
 
+let test3 = tail([1]);
+console.log(test3);
+
+let test4 = tail([]);
+console.log(test4);
