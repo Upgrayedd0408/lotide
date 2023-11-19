b8d2741177013d5c353919320bea7fd87ecffe37
Upgrayedd0408 <michaellowry89@gmail.com>
2023-11-19 06:50:57 +0000

    made AI changes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,25 @@
-const assertArraysEqual = function(arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: arr1.length !== arr2.length`);
+    return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${arr1[i]} !== ${arr2[i]}`);
-      return
+      return false;
     }
   }
-  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
 };
 
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual([1, 2, 3], [3, 2]);
