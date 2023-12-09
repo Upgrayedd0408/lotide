e5a9f1addbfef13214f0f44b03ea751ed885bc21
Upgrayedd0408 <michaellowry89@gmail.com>
2023-12-09 03:09:28 +0000

    created test files to modularize functions

---
+++ test/assertEqualTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Michael Lowry", "Michael Lowry");
+assertEqual("Michael Lowerly", "Michael Lowry");
+assertEqual(1, 1.5);
+
+
+
+
