a3de9abc483644645fe2f93986f9fe12ac2b6713
Upgrayedd0408 <michaellowry89@gmail.com>
2023-12-09 05:19:23 +0000

    made changes suggested by AI

--- middle.js
+++ middle.js
@@ -4,7 +4,7 @@ const middle = function(array) {
     return midValue;
   } else if (array.length % 2 === 0) {
     midValue.push(array[array.length / 2 - 1], array[array.length / 2]);
-  } else if (array.length % 2 !== 0) {
+  } else {
     midValue.push(array[Math.floor(array.length / 2)]);
   }
   return midValue;
