a409ed560c090a8b140d35ad6b31d844168ce64f
Upgrayedd0408 <michaellowry89@gmail.com>
2023-12-09 03:20:11 +0000

    refactored by moving testing code to tailTest file

---
+++ test/tailTest.js
@@ -0,0 +1,24 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+
+//testing to see if tail function doesn't modify original variable
+let fam = ["Buddy", "Maverick", "Myles"];
+tail(fam);
+assertEqual(fam.length, 3);
+
+//testing to see if tail function works correctly.
+let words = tail(["Buddy", "Maverick", "Myles"]);
+assertEqual(words.length, 2);
+assertEqual(words[0], "Maverick");
+assertEqual(words[1], "Myles");
+
+//Testing arrays with only 1 element
+let test3 = tail([1]);
+console.log(test3);
+assertEqual(test3.length, 0);
+
+//Testing empty arrays
+let test4 = tail([]);
+console.log(test4);
+assertEqual(test4.length, 0);
